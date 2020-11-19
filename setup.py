4f5bf7149567016c4bede71f1dcce3eb63796458
Carlos Ruiz <cruiz_perez@hotmail.com>
2020-11-19 14:58:51 -0500

    Added setup.py
    Former-commit-id: ce9a6e5ec29e1f7ff17f1bd98b7a7f32cd98fae8 [formerly 8161ae6f26872c88ec28dd6ad6d6b0be16935cc9]
    Former-commit-id: 1bf2e6f0e7d0bd04bc67af78fcbd9673491143ea

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='microbeannotator',
+    version='1.0.0',
+    description='A user friendly microbe genome annotation tool',
+    url='https://github.com/cruizperez/MicrobeAnnotator',
+    author='Carlos A. Ruiz Perez',
+    author_email='cruizperez3@gatech.edu',
+    packages=find_packages(),
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': [
+            'microbeannotator=microbeannotator:main',
+            'microbeannotator_db_builder=microbeannotator_db_builder:main',
+        ],
+    },
+)
