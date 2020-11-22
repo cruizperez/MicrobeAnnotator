9f02b6ac1dbfa684880ce1dcf2f2f80cc40220bc
cruizperez <cruiz_perez@hotmail.com>
2020-11-22 09:45:08 -0500

    Removed save table
    Former-commit-id: 9a5cc169b068a13fd3bf3491a956d9ac33e6b437 [formerly e7e418ca86b5c0df929165c74e98d75d99912426]
    Former-commit-id: 683300e74da6543634c3f52c3e7b15a0c7673cf6

--- independent_scripts/ko_mapper.py
+++ independent_scripts/ko_mapper.py
@@ -473,7 +473,6 @@ def global_mapper(regular_modules, bifurcating_modules, structural_modules, anno
 def plot_function_barplots(module_colors, module_group_matrix, metabolism_matrix_dropped_relabel, prefix):
     matplotlib.rcParams['pdf.fonttype'] = 42
     print("Grouping by metabolism type and plotting... ")
-    pd.DataFrame.to_csv(metabolism_matrix_dropped_relabel, "SALIDA", sep="\t", header=True, index=True)
     for module in list(metabolism_matrix_dropped_relabel.index):
         for genome in list(metabolism_matrix_dropped_relabel.columns):
             if metabolism_matrix_dropped_relabel.loc[module,genome] >= 80:
