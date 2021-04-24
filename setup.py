a3d5e168f192765c6ea68a7f66e468ae7204fe13
Carlos Ruiz <cruiz_perez@hotmail.com>
2021-04-24 00:38:50 -0400

    Added requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 from microbeannotator import version
+import os
+
+requirements = [req.strip() for req in open('requirements.txt', 'r').readlines()]
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
 setup(
     name='microbeannotator',
     version=version,
-    description='A user friendly microbe genome annotation tool',
-    url='https://github.com/cruizperez/MicrobeAnnotator',
+
+    scripts=['bin/microbeannotator', 'bin/microbeannotator_db_builder'],
+    include_package_data=True,
+
+    packages=find_packages(),
+
+    install_requires = requirements,
+    python_requires='>=3.6, <=3.7.5',
     author='Carlos A. Ruiz Perez',
     author_email='cruizperez3@gatech.edu',
+    description='A user friendly microbe genome annotation tool',
+    licence='Artistic License 2.0',
     keywords = ['genome annotation', 'protein', 'comparative genomics'],
-    packages=find_packages(),
-    include_package_data=True,
-    use_scm_version=False,
-    setup_requires=[
-        'setuptools_scm'],
-    python_requires='>=3.6, <=3.7.5',
-    scripts=['bin/microbeannotator',
-             'bin/microbeannotator_db_builder'] 
+    url='https://github.com/cruizperez/MicrobeAnnotator'
 )
