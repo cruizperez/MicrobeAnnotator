6c470d805c609aaf82416e42981a6398da494d7f
Carlos Ruiz <cruiz_perez@hotmail.com>
2021-04-27 03:16:22 -0400

    Removed requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup, find_packages
 from microbeannotator import version
 import os
 
-requirements = [req.strip() for req in open('requirements.txt', 'r').readlines()]
-
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
@@ -16,12 +14,11 @@ setup(
 
     packages=find_packages(),
 
-    install_requires = requirements,
-    python_requires='>=3.6, <=3.7.5',
+    python_requires='>=3.6, <=3.8',
     author='Carlos A. Ruiz Perez',
     author_email='cruizperez3@gatech.edu',
     description='A user friendly microbe genome annotation tool',
-    licence='Artistic License 2.0',
+    license='Artistic License 2.0',
     keywords = ['genome annotation', 'protein', 'comparative genomics'],
     url='https://github.com/cruizperez/MicrobeAnnotator'
 )
