c980571fbf8ef8d07440e41584757e0ae37afb33
carlos ruiz <cruiz_perez@hotmail.com>
2022-07-18 08:16:10 -0700

    Misc- Fixed formatting.

--- src/microbeannotator/errors.py
+++ src/microbeannotator/errors.py
@@ -5,6 +5,7 @@ class AttributeTypeError(Exception):
     """
     Exception raised when an object is not of the expected type.
     """
+
     def __init__(self, attribute: Any, attr_type: type, message: Optional[str] = None) -> None:
         if not message:
             self.message = f"Attribute <{attribute}> is not of type <{attr_type}>"
