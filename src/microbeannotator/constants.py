7ffefcaa8957e189378f43c541c8892e2792eefa
carlos ruiz <cruiz_perez@hotmail.com>
2022-07-18 00:03:40 -0700

    constants.py - Added logger formats.

---
+++ src/microbeannotator/constants.py
@@ -0,0 +1,8 @@
+# Logger formats
+INFO_FORMAT = "%(asctime)s\t[%(levelname)s]: %(message)s"
+WARNING_FORMAT = "%(asctime)s\t%(name)s - [%(levelname)s]: %(message)s"
+
+# KEGG URLs
+MODULE_JSON_URL = (
+    "https://www.genome.jp/kegg-bin/download_htext?htext=ko00002.keg&format=json&filedir="
+)
